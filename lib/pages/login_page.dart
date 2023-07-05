d19c4c13505d821ef7e17483b86d80c72362507e
arjablc <arjablc@gmail.com>
2023-07-05 16:59:57 +0545

    refactoring and foldered structure

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,40 @@
+// import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '../shared/UI/blurred_container.dart';
+import '../shared/UI/login_form.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.purple, Colors.blue, Colors.purple],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.2, 0.5, 1],
+              ),
+            ),
+          ),
+          Center(
+            child: BlurredContainer(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.6,
+              bgColor: Colors.blue[100],
+              child: const LoginForm(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
